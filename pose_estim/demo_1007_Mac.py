39254dd27630662bb07d57baf29e31f9b449eeec
tenk-9 <49711514+tenk-9@users.noreply.github.com>
2024-10-14 16:50:05 +0900

    mod program

--- pose_estim/demo_1007_Mac.py
+++ pose_estim/demo_1007_Mac.py
@@ -10,20 +10,23 @@ from ultralytics.engine.results import Results
 from BodyInfo import BodyInfo
 
 STRIDE = 32
-SCALE = 12
+SCALE = 10
 IMG_H = STRIDE * int(SCALE / 4 * 3)
 IMG_W = STRIDE * SCALE
 
-MOUSE_MOVE_COEF_H = 1000
-MOUSE_MOVE_COEF_V = 200
+CAM_DEVICE_ID = 1 # 1: outer webcam, 0: stock camera
 
+MOUSE_MOVE_COEF_H = 1.2
+MOUSE_MOVE_COEF_V = 0.5
+
+MOVE_AREA_LIMIT = 0.95 # ウィンドウ内でのマウスの稼働領域を制限
 WINDOW_HEIGHT = 1080
 WINDOW_WIDTH = 1920
 
 # control flags
 stop_flag = False # キー入力を追跡するためのフラグ
 mouse_move_flag = True  # マウス移動のオン/オフを管理するフラグ
-imshow_flag = True # 推論結果の画像を表示するか否かを管理するフラグ
+imshow_flag = False # 推論結果の画像を表示するか否かを管理するフラグ
 
 # フレームキューの作成
 frame_queue = queue.Queue(maxsize=2)  # キューの最大サイズを設定してメモリを節約
@@ -69,8 +72,8 @@ def main(bodyinfo: BodyInfo):
     # マウス移動が有効な場合のみ動かす
     if mouse_move_flag:
         pyautogui.moveTo(
-            2560 / 3 + tilt * MOUSE_MOVE_COEF_H,
-            1600 / 4 - angle * MOUSE_MOVE_COEF_V,
+            (tilt + 1) * MOUSE_MOVE_COEF_H * WINDOW_WIDTH / 2 * MOVE_AREA_LIMIT,
+            (-angle + 1) * MOUSE_MOVE_COEF_V * WINDOW_HEIGHT * MOVE_AREA_LIMIT,
             _pause=False,
         )
     if stop_flag:
@@ -82,6 +85,7 @@ def main(bodyinfo: BodyInfo):
         )
         cv2.imshow('camImg', plot_img)
         cv2.waitKey(1)
+    cv2.waitKey(1)
     return 0
 
 def on_press(key):
@@ -98,8 +102,8 @@ def on_press(key):
 
 print("LOADING MODELS...")
 model = YOLO('./yolo11n-pose.pt')
-print("CONNECTING TO CAMERA...")
-cam = cv2.VideoCapture(0)
+print(f"CONNECTING TO CAMERA {CAM_DEVICE_ID} ...")
+cam = cv2.VideoCapture(CAM_DEVICE_ID)
 
 
 print(f"START INFERENCE ON IMG({IMG_W}, {IMG_H})...")
